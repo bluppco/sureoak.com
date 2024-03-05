7e882300053d9be3e2599bfb0a6f05aecc72d04a
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-03-05 11:29:44 +0530

    feat: basic setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,28 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    integrations: [tailwind({
+
+		applyBaseStyles: false
+
+	}), react(), mdx()],
+  	output: "server",
+  	adapter: cloudflare({
+
+		runtime: {
+
+		  	mode: 'local',
+		  	type: 'pages'
+
+		}
+
+	})
+
+})
